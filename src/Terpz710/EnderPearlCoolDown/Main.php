7e82464473b094ad0c26ef57b5fde14e951135e9
BlueGamesNetwork <142659461+Terpz710@users.noreply.github.com>
2023-10-14 19:12:06 -0700

    Add files via upload

---
+++ src/Terpz710/EnderPearlCoolDown/Main.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Terpz710\EnderPearlCooldown;
+
+use pocketmine\event\Listener;
+use pocketmine\event\entity\ProjectileLaunchEvent;
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase implements Listener {
+    private $cooldowns = [];
+
+    public function onEnable(): void {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    public function onProjectileLaunch(ProjectileLaunchEvent $event) {
+        $projectile = $event->getEntity();
+        if ($projectile instanceof \pocketmine\entity\projectile\EnderPearl) {
+            $player = $projectile->getOwningEntity();
+            if ($player !== null) {
+                $playerName = $player->getName();
+                if (!isset($this->cooldowns[$playerName]) || $this->cooldowns[$playerName] <= microtime(true)) {
+                    $this->cooldowns[$playerName] = microtime(true) + 10;
+                } else {
+                    $remainingTime = ceil($this->cooldowns[$playerName] - microtime(true));
+                    $player->sendMessage("§eYou must wait §c{$remainingTime}§e seconds before using another ender pearl.");
+                    $projectile->kill();
+                    $event->cancel();
+                }
+            }
+        }
+    }
+}
